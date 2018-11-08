bc7fb1f3a5f01dcd9ede6c9ded041d8e44099b96
kaajavi <javierguignard@gmail.com>
2018-11-08 11:31:24 -0300

    Add default port and environment settings

--- api/run.py
+++ api/run.py
@@ -2,8 +2,9 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('APP_SETTINGS')
+config_name = os.getenv('APP_SETTINGS', 'development')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
+)
